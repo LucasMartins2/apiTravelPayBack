ab3a0ad09d7b0eb9982f2d668fca8a02253399f1
Lucas de Paula Martins <lucas.martins.1304979@sga.pucminas.br>
2024-05-22 20:42:06 -0300

    mudando hein

--- Program.cs
+++ Program.cs
@@ -12,12 +12,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MinhaApi v1"));
-}
+
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MinhaApi v1"));
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
